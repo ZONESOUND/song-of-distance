452861ecdd2ec04aa017ca5a11ac35ab72a1da0c
lojoyu <sophie200429@gmail.com>
2019-10-21 18:19:14 +0800

    Updates

---
+++ precache-manifest.e88f93f9d3b637564c546edd144457ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbaa091cf70c70b8b3890940a20cb3d0",
+    "url": "/song-of-distance-swipe/index.html"
+  },
+  {
+    "revision": "3fb9f1a5616ba2e489cd",
+    "url": "/song-of-distance-swipe/static/css/main.7fce7676.chunk.css"
+  },
+  {
+    "revision": "adbda334661254232331",
+    "url": "/song-of-distance-swipe/static/js/2.ee1fd9b6.chunk.js"
+  },
+  {
+    "revision": "3fb9f1a5616ba2e489cd",
+    "url": "/song-of-distance-swipe/static/js/main.1e55db4f.chunk.js"
+  },
+  {
+    "revision": "bbdabcbc48ecd83c7a44",
+    "url": "/song-of-distance-swipe/static/js/runtime-main.10d7d4db.js"
+  }
+]);
