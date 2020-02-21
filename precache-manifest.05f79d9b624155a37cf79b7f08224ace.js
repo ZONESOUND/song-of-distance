6409fd76a5921b09ea8807a3487420843df42454
lojoyu <sophie200429@gmail.com>
2020-02-21 17:53:06 +0800

    Updates

---
+++ precache-manifest.05f79d9b624155a37cf79b7f08224ace.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2eb7e24b56c49b3d409310d701743b",
+    "url": "/song-of-distance/index.html"
+  },
+  {
+    "revision": "9f04475d34c72a57f090",
+    "url": "/song-of-distance/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "300f98f37d11e3832598",
+    "url": "/song-of-distance/static/css/main.327d5dd1.chunk.css"
+  },
+  {
+    "revision": "9f04475d34c72a57f090",
+    "url": "/song-of-distance/static/js/2.c427cbc1.chunk.js"
+  },
+  {
+    "revision": "300f98f37d11e3832598",
+    "url": "/song-of-distance/static/js/main.6c2a3d15.chunk.js"
+  },
+  {
+    "revision": "c22877b491ea1a9af96d",
+    "url": "/song-of-distance/static/js/runtime-main.3e03df9d.js"
+  },
+  {
+    "revision": "43d7547be834e73a9c004ccf822aaf62",
+    "url": "/song-of-distance/static/media/Settings-icon.43d7547b.png"
+  }
+]);
