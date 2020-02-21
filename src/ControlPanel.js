9cefa5b6b2e42bc60b78a391378af6eb2edf39a5
lojoyu <sophie200429@gmail.com>
2020-02-21 12:07:39 +0800

    uncomment name modal

--- src/ControlPanel.js
+++ src/ControlPanel.js
@@ -188,8 +188,8 @@ class ControlPanel extends Component {
 
         return (
             <>
-            {/* <NameModal show={this.state.naming} name={data.centerName} 
-                        onChange={this.changeCenterName}/> */}
+            <NameModal show={this.state.naming} name={data.centerName} 
+                        onChange={this.changeCenterName}/>
             <P5Wrapper sketch={sketch} dataPoint={dataPoint}
                     configData={data} myId={gpsData.key}/>
             </>
