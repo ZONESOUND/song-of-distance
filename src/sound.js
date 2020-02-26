208bb3da67ba916cdfe184c85b658c697af3361c
lojoyu <sophie200429@gmail.com>
2020-02-26 18:00:24 +0800

    to function

--- src/sound.js
+++ src/sound.js
@@ -2,6 +2,7 @@ import Tone from 'tone';
 let octave = 'CDEFGAB'; //start from index 1 so put C at second 
 let octaveStart = 2;
 let octaveMax = 6;
+let noteDuration = 150;
 let synth, reverb, comp;
 let noteTimeout = {};
 let noteSynth = {};
@@ -12,21 +13,9 @@ export let initSound = () => {
         pre_delay: 0.05,
         decay: 3,
         wet: 0.6,
-    });
+    }).connect(comp);
     reverb.generate();
-    for (let i=0; i<octave.length; i++) {
-        for (let j=octaveStart; j<=octaveMax; j++) {
-            noteSynth[octave[i]+j] = new Tone.Synth({
-                oscillator : {type:'sine'} ,
-                envelope : {
-                    attack: 0.1 ,
-                    decay: 5 ,
-                    sustain: 0.2 ,
-                    release: 0.5
-                }});
-            noteSynth[octave[i]+j].envelope.decayCurve = 'exponential';
-        }
-    }
+    initNote();
     // synth = new Tone.Synth({
     //     oscillator : {type:'sine'} ,
     //     envelope : {
@@ -38,13 +27,36 @@ export let initSound = () => {
         //.chain(filter, reverb);
 }
 
+let initNote = () => {
+    for (let i=0; i<octave.length; i++) {
+        for (let j=octaveStart; j<=octaveMax; j++) {
+            noteSynth[octave[i]+j] = new Tone.Synth({
+                oscillator : {type:'sine'} ,
+                envelope : {
+                    attack: 0.1 ,
+                    decay: 5 ,
+                    sustain: 0.2 ,
+                    release: 0.5
+                }});
+            noteSynth[octave[i]+j].envelope.decayCurve = 'exponential';
+            // noteSynth[octave[i]+j] = new Tone.FMSynth({envelope:{
+            //     attack: 0.1 ,
+            //     decay: 5 ,
+            //     sustain: 0.2 ,
+            //     release: 0.5
+            // }});
+            // noteSynth[octave[i]+j].envelope.decayCurve = 'exponential';
+        }
+    }
+}
+
 export let triggerSound = (d) => {
     let layer = Math.ceil(d.layer);
     let dis = Math.max(0.5, layer - d.layer);
     let note = getNote(layer);
     if (noteTimeout[note]) {
         clearTimeout(noteTimeout[note]);
-        setTimeout(()=>{setRelease(note)}, 150);
+        setTimeout(()=>{setRelease(note)}, noteDuration);
         return;
     }
     
@@ -54,15 +66,15 @@ export let triggerSound = (d) => {
     
     // var fmSynth = new Tone.FMSynth().chain(filter, reverb, comp);
     // fmSynth.triggerAttackRelease(note, 0.15);
-    console.log(noteSynth[note]);
+    setAttackRelease(filter, note);
+}
 
-    //let newSynth = noteSynth[note].chain(filter, reverb, comp);
-    //newSynth.triggerAttack(note);
-    (noteSynth[note].chain(filter, reverb, comp)).triggerAttack(note);
-    
+let setAttackRelease = (filter, note) => {
+    //(noteSynth[note].chain(filter, reverb, comp)).triggerAttack(note);
+    (noteSynth[note].chain(filter,reverb)).triggerAttack(note);
     noteTimeout[note] = setTimeout(()=>{
         setRelease(note);
-    }, 150);
+    }, noteDuration);
 }
 
 let setRelease = (note) => {
