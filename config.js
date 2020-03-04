8418321648c7573d720b544522da35968ad52e10
stu84096 <stu84096@gmail.com>
2020-03-04 17:26:01 -0500

    do nothing

---
+++ config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+    //put firebase config here
+    apiKey: "AIzaSyBlfFaGBx7B_nBnlR29yhJAr6kPjqAdLfo",
+    authDomain: "test-84ae7.firebaseapp.com",
+    databaseURL: "https://test-84ae7.firebaseio.com",
+    projectId: "test-84ae7",
+    storageBucket: "test-84ae7.appspot.com",
+    messagingSenderId: "762464277938"
+};
+export const serverIP = 'https://gps-250305.appspot.com'; // change IP here
+//https://gps-250305.appspot.com
